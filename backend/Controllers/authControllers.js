6f30f81b2a6c226a6844a18fda5ff8c104241c57
Julian Parada <jparadaa1707@gmail.com>
2026-02-12 19:54:51 -0500

    Julian Parada
    authControllers created

--- backend/Controllers/authControllers.js
+++ backend/Controllers/authControllers.js
@@ -64,4 +64,99 @@ exports.signup = async (req, res) => {
     }
 };
 
+/** 
+ * SIGNIN : iniciar sesion
+ * POST /api/auth/signin
+ * Body {email o usuario, password}
+ * busca el usuario por email o username
+ * valida la contraseña con bcrypt
+ * si es correcto el token JWT 
+ * token se usa para autenticar futuras solicitudes
+ */
 
+exports.signin = async (req, res) => {
+    try{
+        //valida que se envie el email o username
+        if (!req.body.email && !req.body.username){
+            return res.status(400).json({
+                sucess: false,
+                message: 'email o username requerido'
+            });
+        }
+
+        //validar que se envie la contraseña
+        if (!req.body.password){
+            return res.status(400).json({
+                sucess: false,
+                message: 'pasword requerido'
+            });
+        }
+
+        //buscar el usuario por email o username
+        const user = await User.findOne({
+            $or: [
+                { username: req.body.username }
+                { email: req.body.email }
+            ]
+        }).select('+password'); // incluye password field
+    
+        //si no enxiste el uduario con este email o username
+        if (!user){
+            return res.status(404).json({
+                sucess: false,
+                message: 'Usuario no encontrado'
+            });
+        }
+
+        //verificar que el usuario tenga contraseña
+        if (!user.password){
+            return res.status(500).json({
+                sucess: false,
+                message: 'Error interno: usuario sin contraseña'
+            });
+        }
+
+        //comparar la contraseña enviada con el hash almacenado
+        const passwordIsValid = await bcrypt.compare
+        (req.body.password, user.password);
+
+        if(!passwordIsValid){
+            return res.status(401).json({
+                sucess: false,
+                message: 'Contraseña incorrecta'
+            });
+        }
+
+        //generar token JWT 24 horas
+        const token = jwt.sign(
+            { 
+                id: user._id,
+                role: user.role,
+                email: user.email
+            },
+            config.secret,
+            { expiresIn: config.jwtExpiration }
+        );
+
+        //preparar respuesta sin mostrar la contraseña
+        const userResponse = {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role,
+        };
+
+        res.status(200).json({
+            sucess: true,
+            message: 'Inicio de sesión exitoso',
+            token: token,
+            user: userResponse
+        });
+    }  catch (error){
+        return res.status(500).json({
+            sucess: false,
+            message: 'Error al iniciar sesión',
+            error: error.message
+        });
+    }
+}
